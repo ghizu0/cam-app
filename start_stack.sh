d8f1e95f5e714a63526c853e6c40cfb4fe1a88d3
Gonzalo <bravomoyanogonzalo@gmail.com>
2022-01-20 13:36:52 +0100

    .

--- start_stack.sh
+++ start_stack.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 cd /home/ubuntu/cam-app
-sudo docker stack deploy -c docker-compose.yml cam-app
+
+read -p "Introduce c (muestra imagenes), p (muestra poses) o f (toma imagenes de fondo): " modo
+read -p "¿Cuantas horas va a estar en modo c o p? " horas
+
+export compose_modo=$modo
+export compose_horas=$horas
+
+envsubst < docker-compose.yml > docker-compose-variables.yml
+
+sudo docker stack deploy -c docker-compose-variables.yml cam-app
